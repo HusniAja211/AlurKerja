0489ce4bbaa6f3ba7623cd82df05000d467ce6af
Satriafarel <satriafarel40@gmail.com>
2025-07-17 11:15:37 +0700

    Desain Login ya bro

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import "./../index.css";
+import { FcGoogle } from "react-icons/fc";
+import logoWeb from "./../assets/Logo.png";
 
 function Login() {
   return (
@@ -8,41 +10,98 @@ function Login() {
         className="flex flex-col items-center justify-center text-black h-screen bg-gradient-to-r from-blue-500 to-blue-800"
         style={{ width: "100%" }}
       >
-        <div className="bg-white p-8 rounded-lg shadow-md">
-          <h1 className="text-2xl font-bold mb-4">Login</h1>
-          <form>
-            <div className="mb-4">
-              <label className="block text-sm font-medium mb-2" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="border border-gray-300 p-2 w-full rounded"
-                type="email"
-                id="email"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-sm font-medium mb-2"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                className="border border-gray-300 p-2 w-full rounded"
-                type="password"
-                id="password"
-                required
-              />
-            </div>
-            <button
-              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-              type="submit"
+        <div className="bg-white flex p-10 rounded-3xl shadow-md w-1/3">
+          <div className="flex flex-col w-full">
+            <img src={logoWeb} alt="Logo" className="mb-4 w-32" />
+            <h2
+              className="font-bold my-5 text-center"
+              style={{ fontSize: "2rem" }}
             >
-              Login
-            </button>
-          </form>
+              Masuk Akun Anda
+            </h2>
+            <form>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="border border-gray-300 p-2 rounded"
+                  type="email"
+                  id="email"
+                  style={{ width: "100%" }}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="border border-gray-300 p-2 w-full rounded"
+                  type="password"
+                  id="password"
+                  style={{ width: "100%" }}
+                  required
+                />
+              </div>
+              <div className="flex justify-between items-center mb-4">
+                <div className="flex items-center">
+                  <input type="checkbox" id="remember" className="mr-2" />
+                  <label htmlFor="remember">Ingat Saya</label>
+                </div>
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-500 hover:underline"
+                >
+                  Lupa Password?
+                </Link>
+              </div>
+              <div className="flex justify-center flex-col items-center">
+                <button
+                  className="text-white hover:text-blue-300 btn-masuk text-center"
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(90deg, #3164ff 0%, #1d3c99 100%)",
+                    color: "white",
+                    padding: "0.50rem 2rem",
+                    borderRadius: "2rem",
+                    fontWeight: 600,
+                    transition: "opacity 0.3s ease",
+                  }}
+                >
+                  Masuk
+                </button>
+                <p className="text-center mt-2">
+                  Belum punya akun?{" "}
+                  <Link to="/register" className="text-blue-900">
+                    Daftar
+                  </Link>
+                </p>
+                <div className="flex text-center flex-col justify-center my-2">
+                  <p className="text-center">atau</p>
+                </div>
+                <button
+                  className="flex justify-center items-center gap-2 text-white hover:text-blue-300 btn-daftar text-center mx-auto"
+                  style={{
+                    backgroundColor: "#f0f0f0",
+                    color: "#3164ff",
+                    border: "1px solid #3164ff",
+                    padding: "0.50rem 2rem",
+                    borderRadius: "2rem",
+                    transition: "opacity 0.3s ease",
+                  }}
+                >
+                  <FcGoogle size={20} /> Lanjutkan dengan Google
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
